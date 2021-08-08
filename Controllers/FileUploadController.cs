2a07c18f676c0c5761778a47ead90671cdc41479
Emmanuel Vitus <emmanuelvitus01@gmail.com>
2021-08-07 19:41:01 -0700

    Project Function Acheived
    This app basic function (Saving Image) is complete

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageuploadingDemo.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        // GET: FileUpload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFiles(HttpPostedFileBase file)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //get file details from the posted file
+                    if (file != null)
+                    {
+                        string path = Path.Combine(Server.MapPath("~/UploadedFiles"), Path.GetFileName(file.FileName));
+                        file.SaveAs(path);
+
+                        TempData["msg"] = "File uploaded successfully.";
+                    }
+
+                    
+
+                }
+                catch (Exception)
+                {
+                    TempData["msg"] = "Error while uploading files.";
+                }
+            }
+            return View("Index");
+        }
+    }
+}
